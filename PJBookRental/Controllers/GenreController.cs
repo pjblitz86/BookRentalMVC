1c9b11655f196d9cdabb1b36f36ec6dfedd78aa2
Paulius Juknis <paulius.juknis@gmail.com>
2018-07-18 15:22:41 +0300

    Adding Genre logic and partial views

---
+++ PJBookRental/Controllers/GenreController.cs
@@ -0,0 +1,65 @@
+﻿using PJBookRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PJBookRental.Controllers
+{
+    public class GenreController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public GenreController()
+        {
+            db = new ApplicationDbContext();
+        }
+        // GET: Genre
+        public ActionResult Index()
+        {
+            return View(db.Genres.ToList());
+        }
+
+        // Get Action
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Genre genre)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Genres.Add(genre);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Genre genre = db.Genres.Find(id);
+            if(genre == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(genre);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+        }
+    }
+}
