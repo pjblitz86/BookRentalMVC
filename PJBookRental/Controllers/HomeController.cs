6d0044afe4f02e239a8f3dae3f6dccae97ab38e5
Paulius Juknis <paulius.juknis@gmail.com>
2018-07-20 11:23:05 +0300

    Created Index view for HomeController using Thumbnail model

--- PJBookRental/Controllers/HomeController.cs
+++ PJBookRental/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PJBookRental.ViewModel;
+using PJBookRental.Extensions;
+using PJBookRental.Models;
 
 namespace PJBookRental.Controllers
 {
@@ -10,7 +13,19 @@ namespace PJBookRental.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var thumbnails = new List<ThumbnailModel>().GetBookThumbnail(ApplicationDbContext.Create());
+            var count = thumbnails.Count() / 4;
+            var model = new List<ThumbnailBoxViewModel>();
+
+            for(int i = 0; i<=count; i++)
+            {
+                model.Add(new ThumbnailBoxViewModel
+                {
+                    Thumbnails = thumbnails.Skip(i*4).Take(4)
+                });
+            }
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ PJBookRental/Extensions/ThumbnailExtension.cs
@@ -0,0 +1,35 @@
+﻿using PJBookRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PJBookRental.Extensions
+{
+    public static class ThumbnailExtension
+    {
+        public static IEnumerable<ThumbnailModel> GetBookThumbnail(this List<ThumbnailModel> thumbnails, ApplicationDbContext db=null)
+        {
+            try
+            {
+
+            if(db==null)
+            {
+                db = ApplicationDbContext.Create();
+            }
+
+            thumbnails = (from b in db.Books
+                          select new ThumbnailModel
+                          {
+                              BookId = b.Id,
+                              Title = b.Title,
+                              Description = b.Description,
+                              ImageUrl = b.ImageUrl,
+                              Link = "/BookDetail/Index/" + b.Id
+                          }).ToList();
+            }
+            catch(Exception ex) { }
+            return thumbnails.OrderBy(b => b.Title);
+        }
+    }
+}
